14308e5ec012a6b365a0375b6d4abb2a8c066abd
Alvin Lin <alvinch.lin@gmail.com>
2021-04-14 04:20:04 +0000

    Add log in textbox for username input. Add log in button. Add the display of username instead of input box when logged in. Add log out button.

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const generateRandomString = () => {
   // return a string of 6 random alphaNumeric characters
@@ -18,17 +19,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  // Home
-  const templateIndex = { urls: urlDatabase};
-  res.render('urls_index',templateIndex);
-});
+// app.get("/", (req, res) => {
+//   // Home
+//   const templateIndex = { urls: urlDatabase};
+//   res.render('urls_index',templateIndex);
+// });
 
 app.get("/urls", (req, res) => {
-  const templateIndex = { urls: urlDatabase};
-  res.render('urls_index',templateIndex);
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render('urls_index',templateVars);
 });
 
 app.post("/urls", (req, res)=> {
@@ -41,7 +46,10 @@ app.post("/urls", (req, res)=> {
 })
 
 app.get("/urls/new", (req, res) => {
-  res.render('urls_new');
+  const templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render('urls_new', templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -54,13 +62,28 @@ app.get("/urls/:shortURL", (req, res) => {
   // above value behind ":", in this case, "shortURL" is populated to req.param object as the key and the actual value passed in becomes the value: eg. req.params = {shortURL: "b2xVn2"}
   const templateShow = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
   }
   res.render('urls_show', templateShow);
 })
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+})
+
+app.post('/login', (req, res) => {
+  const usernameValue = req.body.username;
+  const username = Object.keys(req.body);
+  //set a cookie named username to the value submitted in the request body via the login form
+  res.cookie(username, usernameValue);
+  // redirect back to the /urls
+  res.redirect('/urls');
+})
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
   res.redirect('/urls');
 })
 
