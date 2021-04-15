2271c52fd4e3a03eb40028902637cb313827ed70
Alvin Lin <alvinch.lin@gmail.com>
2021-04-15 05:58:45 +0000

    Add basic permission features. Add more permission features such as user can only see their own urls and edit/delete their urls.

--- express_server.js
+++ express_server.js
@@ -22,21 +22,40 @@ const databaseCheck = (testValue, databaseProperty) => {
   }
   return false;
 }
+const urlsForUser = (id) => {
+  // return URLs when current user ID matches the userID on the URL
+  let userUrlDatabase = {};
+  for (const url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      userUrlDatabase[url] = urlDatabase[url];
+    }
+  }
+  return userUrlDatabase;
+}
+
 // Database objects
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "E7HRz3" },
+  "9sm5xK": { longURL: "http://www.google.com", userID: "E7HRz3" },
+  "cBYvji": { longURL: "http://www.youtube.com", userID: "0w1dxG" },
+  "cnJMLf": { longURL: "http://www.getbootstrap.com", userID: "0w1dxG" },
+  "R9tO59": { longURL: "http://www.pomofocus.io", userID: "j22L2z" }
 };
 const users = {
-  "userRandomID": {
-    id: "userRandomID",
+  "j22L2z": {
+    id: "j22L2z",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password: "1234"
   },
   "E7HRz3": {
     id: "E7HRz3",
     email: "aloha1234@nomail.com",
     password: "qwerty"
+  },
+  "0w1dxG": {
+    id: "0w1dxG",
+    email: "pineapple123@nomail.com",
+    password: "asdf"
   }
 }
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -45,19 +64,30 @@ app.set('view engine', 'ejs');
 
 // app.get("/", (req, res) => {
 //   // Home
-//   const templateIndex = { urls: urlDatabase};
-//   res.render('urls_index',templateIndex);
+//   const templateIndex = { urls: urlDatabase };
+//   res.render('urls_index', templateIndex);
 // });
+const loggedIn = (reqCookie) => {
+  return (Object.keys(reqCookie).length !== 0);
+}
 // Main page
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    user: users[req.cookies["user_id"]]
+    user: users[req.cookies["user_id"]],
+    cookie: req.cookies
   };
+  if (loggedIn(req.cookies)){
+    templateVars.urls = urlsForUser(req.cookies['user_id']);
+  }
   res.render('urls_index', templateVars);
 });
 // Create shortURL page
 app.get("/urls/new", (req, res) => {
+  // if not logged in. redirect to /login
+  if (Object.keys(req.cookies).length === 0) {
+    return res.redirect("/login");
+  }
   const templateVars = {
     user: users[req.cookies["user_id"]]
   }
@@ -66,7 +96,7 @@ app.get("/urls/new", (req, res) => {
 // Handle the post request after the user submit their longURL
 app.post("/urls", (req, res) => {
   console.log(req.body.longURL); // bodyParser processed the body and gave us the decoded content in the form of object and throw it into the req.body
-  urlDatabase[generateRandomString()] = `http://${req.body.longURL}`;
+  urlDatabase[generateRandomString()] = { "longURL": `http://${req.body.longURL}`, "userID": req.cookies["user_id"] };
   console.log('new urlDatabase obj', urlDatabase);
   const keysArr = Object.keys(urlDatabase);
   const key = keysArr[keysArr.length - 1];
@@ -80,11 +110,15 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[req.params.shortURL],
     user: users[req.cookies["user_id"]]
   }
-  res.render('urls_show', templateShow);
+  if (loggedIn(req.cookies)) {
+    return res.render('urls_show', templateShow);
+  }
+  res.redirect('/urls');
 });
 // Redirect to the source url page
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  console.log(urlDatabase[req.params.shortURL]);
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 // Update URL
@@ -94,8 +128,13 @@ app.post('/urls/:shortURL', (req, res) => {
 });
 // Delete
 app.post('/urls/:shortURL/delete', (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect('/urls');
+  // if url user id match with current user id
+  if (req.cookies['user_id'] === req.params.shortURL) {
+    delete urlDatabase[req.params.shortURL];
+    return res.redirect('/urls');
+  }
+  res.status(403);
+  res.end("You have no permission to delete this URL!!")
 });
 // Login
 app.get('/login', (req, res) => {
@@ -144,7 +183,7 @@ app.post('/register', (req, res) => {
     res.end();
   }
   // if duplicated email, return 400
-  const existingUser = databaseCheck(newEmail,"email");
+  const existingUser = databaseCheck(newEmail, "email");
   if (existingUser) {
     console.log('This new user exists', existingUser);
     res.status(400);
